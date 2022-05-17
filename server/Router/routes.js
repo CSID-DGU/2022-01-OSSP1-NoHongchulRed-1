884a2604083d7675e55683fa58f3c3ecf118f09b
ranief87 <62322084+ranief87@users.noreply.github.com>
2022-05-17 16:18:53 +0900

    #6 로그인함수

--- server/Router/routes.js
+++ server/Router/routes.js
@@ -1,27 +1,42 @@
 const express = require('express');
-//const path = require('path');
+const path = require('path');
 const pool = require('../pool');
 
 const router = express.Router();
 
+const bcrypt = require('bcrypt');
+
+const session = require('express-session');
+const res = require('express/lib/response');
 //const index = path.join(__dirname, '../client/build/index.html');
 
 // Authentication
-router.post('/db/users/login',(req,res) => {
-    let {id, password} = req.body;
-
-    pool.query('SELECT * FROM BOOKWEB.UserTB WHERE id =?',id,err => {
-        if (err) {
-            return res.render('error',{message: "로그인 에러"})
+// 로그인 
+// id에 해당하는 유저가 있는지 찾고 비밀번호 암호화 후 값을 비교해서 비번까지 맞는지 확인
+// 성공하면 세션에 유저 정보 담아서 프론트로 넘기고
+router.post('/db/users/login',(req,res) => { 
+    const userId = req.id;
+    const password = req.password;
+    password = bcrypt.hashSync(password); // 암호화
+    
+    pool.query('SELECT * FROM BOOKWEB.UserTB WHERE id =?',[userId],(err,userinfo) => {// id 찾기 
+        if (err || !userinfo[0]) {
+            bcrypt.compare(password, userinfo[0].password,(err,tf) => { // 암호화된 비번 비교
+                if (tf !== true) {
+                    return res.render('error',{message:"아이디 또는 비밀번호 확인해주세요."})
+                }
+                else {
+                    req.session.nickname=userinfo[0].nickname;
+                    req.session.id = userinfo[0].id;
+                    return res.redirect('/');
+                }
+            })
         }
-        else {
-            return res.redirect('/');
-        }
-     });
+    })
 });
 
 // Registration
-
+// id가 중복인지 체크하고 없으면 데이터 값 가지고 insert
 router.post('/db/users',(req,res) => {
     const userId = req.id;
     const password = req.password;
@@ -29,7 +44,8 @@ router.post('/db/users',(req,res) => {
     const age = req.age;
     const sexuality = req.sexuality;
     
-    
+    password= bcrypt.hashSync(password);// 암호화
+             // bcrypt가 hash 된 것끼리 비교하는 함수 있어서
     pool.query('SELECT * FROM BOOKWEB.UserTB WHERE id=? ?',[id],(err) => {
         if (err) {
             console.log(err)
@@ -55,7 +71,7 @@ router.get('/db/:userId', async (req, res, next) => {
     } catch (err) {
         return res.status(500).json(err)
     }
-})
+});
 
 /*
 router.get('*', (req, res) => {
