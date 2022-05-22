9eeff6966e08038063ace12ab608ccb46fb4cb83
ranief87 <62322084+ranief87@users.noreply.github.com>
2022-05-22 21:36:03 +0900

    #15 bookdata 완성

--- client/src/TestDB.js
+++ client/src/TestDB.js
@@ -185,17 +185,15 @@ const Test = () => {
     }
 
     // get book
-    const [userId, setuserId] = useState('');
+    const [isbn, setisbn] = useState('');
 
-    const onChangeUser = (e) => {
-        setuserId(e.target.value);
+    const onChangeisbn = (e) => {
+        setisbn(e.target.value);
     };
 
-    const onSubmitUser = () => {
+    const onSubmitisbn = () => {
         try {
-          // axios로 get
-          // 파라미터 userId로 전달
-            axios.get('/db/users/' + userId)
+            axios.get('/db/books/' + isbn)
             .then((res) => {
                 return res.data;
             })
@@ -207,50 +205,7 @@ const Test = () => {
         }
     }
 
-    const [gbookInputs, setgbookInputs] = useState({
-        isbn: '',
-        title: '',
-        authors: '',
-        publisher: '',
-        thumbnail: ''
-    });
-
-    const onChangegbook = (e) => {
-        const { value, name } = e.target;
-        
-        setgbookInputs({
-            ...gbookInputs,
-            [name]: value
-        });
-    };
-
-    const onSubmitgbook = () => {
-        try {
-            axios.post('/db/books', {
-                isbn: gbookInputs.id,
-                title: gbookInputs.password,
-                authors: gbookInputs.authors,
-                publisher: gbookInputs.publisher,
-                thumbnail: gbookInputs.thumbnail
-            }).then((res) => {
-                return res.data;
-            })
-            .then((data) => {
-                // 세션을 data로 넘겨주었으므로 해당 내용으로 설정
-                console.log(data)
-                setBookSession({
-                    ...bookSession,
-                    isbn: data.id,
-                    title: data.password,
-                    authors: data.authors,
-                    publisher: data.publisher,
-                    thumbnail: data.thumbnail
-                });
-            });
-        } catch (err) {
-            console.log(err)
-        }
-    }
+    
     // create book report
     // get book report
 
@@ -305,18 +260,11 @@ const Test = () => {
             <br />
             <button onClick={onSubmitBook}>책 등록</button>
             <hr />
-
             <h1><font size="3">책 등록 확인</font></h1>
-            <input type="text" name="isbn" placeholder="10자" value={gbookInputs.isbn} maxLength="16" onChange={onChangegbook} />
+            <input type="text" name="isbn" placeholder="isbn 입력" value={isbn} maxLength="16" onChange={onChangeisbn} />
+            <button onClick={onSubmitisbn}>책 등록 확인</button>
             <br />
-            <input type="text" name="title" placeholder="제목 입력하세요" value={gbookInputs.title} maxLength="16" onChange={onChangegbook} />
-            <br />
-            <input type="text" name="authors" placeholder="작가 입력하세요" value={gbookInputs.authors} maxLength="10" onChange={onChangegbook} />
-            <br />
-            <button onClick={onSubmitgbook}>책 등록 확인</button>
-            <br />
-            <p>isbn: {bookSession.isbn}, title: {bookSession.title}, authors: {bookSession.authors}, publisher: {bookSession.publisher}, thumbnail: {bookSession.thumbnail}</p>
-            <br />
+            
 
         </div>
     );
