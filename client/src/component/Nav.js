bfe80d6e01b9ce443416df7a48b3fe1f75f2c888
lwef <linapple1234@icloud.com>
2022-05-23 01:42:57 +0900

    #24 병합1

--- client/src/component/Nav.js
+++ client/src/component/Nav.js
@@ -6,12 +6,14 @@ const Nav = () => {
     const navigate = useNavigate();
 
     const path = window.location.pathname;
-
+    if (path === '/' || path==='/SignUp') return null; /*로그인, 회원가입 페이지에서 Nav 숨기기 */
     return (
         <div className="nav-area">
+            <div style={{fontWeight: path === '/Main' && 'bold' }} onClick={() => navigate('/Main')}>메인페이지</div>
             <div style={{fontWeight: path === '/ShortReport' && 'bold' }} onClick={() => navigate('/ShortReport')}>모든 독후감</div>
-            <div style={{fontWeight: path === '/RecommendPage' && 'bold' }} onClick={() => navigate('/RecommendPage')}>추천 도서</div>
+            <div style={{fontWeight: path === '/BookSearchPage' && 'bold' }} onClick={() => navigate('/BookSearchPage')}>추천 도서</div>
             <div style={{fontWeight: path === '/MyBookPage' && 'bold' }} onClick={() => navigate('/MyBookPage')}>나의 독후감</div>
+            <div style={{fontWeight: path === '/UserEdit' && 'bold' }} onClick={() => navigate('/UserEdit')}>회원정보 수정</div>
         </div>
     )
 };
