66ccb318462e58c772b4b7eabd18605aac9b7a47
homubee <83688807+homubee@users.noreply.github.com>
2022-05-26 15:00:38 +0900

    #30 Add DB API skeleton code

--- server/Router/routes.js
+++ server/Router/routes.js
@@ -58,7 +58,7 @@ router.post('/db/users/login', async (req,res) => {
 // 로그아웃
 router.get('/db/users/logout', async (req,res) => {
     if (req.session.userId) {
-        await req.session.destroy(function(err){
+        await req.session.destroy(function(err) {
             if (err) throw err;
         });
         return res.json({issuccess: true, message: "success"});
@@ -147,23 +147,36 @@ router.get('/db/users/:userId', async (req, res) => {
 router.get('/db/books/:isbn', async (req, res) => {
     const {isbn} = req.params;
     try {
-        const bodata= await pool.query('SELECT * FROM BOOKWEB.BookTB WHERE isbn = ?',[isbn]);
+        const bodata = await pool.query('SELECT * FROM BOOKWEB.BookTB WHERE isbn = ?', [isbn]);
         return res.json(bodata[0][0]);
     } catch (err) {
         return res.status(500).json(err);
     }
 });
 
-// Get Book report
-// 독후감 정보 가져오기
+// Get Book report 1
+// 독후감 정보 가져오기(isbn 기준)
 router.get('/db/bookreports/:isbn', async (req, res) => {
     const {isbn} = req.params; 
     try {
-        const redata= await pool.query('SELECT * FROM BOOKWEB.BookReportTB WHERE isbn = ?',[isbn]);
-        return res.json(redata[0][0]);
+        const redata = await pool.query('SELECT * FROM BOOKWEB.BookReportTB WHERE isbn = ?', [isbn]);
+        return res.json(redata[0]);
     } catch (err) {
         return res.status(500).json(err);
     }
+});
+
+// Get Book report 2
+// 독후감 정보 가져오기(userid 기준)
+router.get('/db/bookreports/:userid', async (req, res) => {
+    // 내용 구현 필요
+});
+
+// Get Book report 3
+// 독후감 정보 가져오기(isbn, userid 기준 - 한 개만 선택됨)
+router.get('/db/bookreports/:isbn/:userid', async (req, res) => {
+    // 내용 구현 필요
+    // 고유한 독후감 정보를 가져오는 것은 단일 독후감 게시물을 읽을 때이므로 조회수에 해당하는 views 값을 하나 증가시켜 update해줘야 함
 });
 
 /*
