871350b9b50c4f65f8634d87c60a6341c74534e7
homubee <83688807+homubee@users.noreply.github.com>
2022-06-11 12:59:17 +0900

    #73 Use constant for parameters

--- server/python/main.py
+++ server/python/main.py
@@ -7,27 +7,28 @@ from sklearn.decomposition import TruncatedSVD
 np.set_printoptions(precision=3, suppress=True)
 
 # 인자로 데이터 받아옴
-R = json.loads(sys.argv[1])
+r = json.loads(sys.argv[1])
 
-# 원본 행렬 R 생성
-R = np.array(R)
+# 원본 행렬 r 생성
+r = np.array(r)
 
 # 예측해야 할 도서 인덱스 저장(점수가 0인 것)
 nonDatas = []
-for i, rate in enumerate(R[len(R)-1]):
+for i, rate in enumerate(r[len(r)-1]):
     if (rate == 0):
            nonDatas.append(i)
 
 # Simple SVD
-u, s, vh = np.linalg.svd(R, full_matrices=False)
+u, s, vh = np.linalg.svd(r, full_matrices=False)
 
-# 특이값 개수(잠재요인 차원) K는 2 설정
-svd = TruncatedSVD(n_components=2)
-svd.fit(R)
+# 특이값 개수(잠재요인 차원) K는 2 설정, K 값 변경하여 자유롭게 테스트해볼 것
+K = 2
+svd = TruncatedSVD(n_components=K)
+svd.fit(r)
 
-u = u[:, :2]
+u = u[:, :K]
 s = svd.singular_values_
-vh = vh[:2, :]
+vh = vh[:K, :]
 
 # 예측 평점 행렬 계산
 us = np.matmul(u, np.diag(s))
@@ -51,9 +52,13 @@ for i, rate in enumerate(result[len(result)-1]):
 # 평점 순 정렬
 recommand.sort(key=lambda x:x[1], reverse=True)
 
-# 인덱스만 추출
+# 인덱스만 추출, 책 3권까지만(NUMOFBOOK)
+NUMOFBOOK = 3
 ret = []
-for element in recommand:
-    ret.append(element[0])
+for i, element in enumerate(recommand):
+    if (i < NUMOFBOOK):
+        ret.append(element[0])
+    else:
+        break
 
 print(ret)
